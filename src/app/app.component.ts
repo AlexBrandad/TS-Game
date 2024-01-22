6989aaad635a4d7a8a3ac1fed6a4d1081b097b37
Philipasdf <ng.philip.91@gmail.com>
2024-01-22 22:58:45 +0100

    spielfeld gebaut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,17 @@ import { PokemonComponent } from './pokemon/pokemon.component';
 })
 export class AppComponent {
   rayquaza = new Pokemon(384, 'Rayquaza');
+
+  feld = {
+    x: 40,
+    y: 30,
+  };
+
+  gridColumns: string = `repeat(${this.feld.x}, 96px)`;
+  gridRows: string = `repeat(${this.feld.y}, 96px)`;
+
+  // pokemon coord x y
+  // feld hat ne achse x y
+
+  // wenn pkmn x=2 y=3
 }
